59b091a44eed08daa194def74e777f93af22698f
Eduardo Pelizzari de Andrade <eduardoandrade@persoft.com.br>
2026-01-05 22:17:43 -0300

    comunicação nfse

---
+++ pynfe/processamento/nfse/comunicacao.py
@@ -0,0 +1,52 @@
+import requests
+from pynfe.entidades.certificado import CertificadoA1
+
+
+class Comunicacao(object):
+
+    def __init__(self, certificado, certificado_senha, homologacao=False):
+        self._certificado = certificado
+        self._certificado_senha = certificado_senha
+        self._homologacao = homologacao
+
+    def enviar_lote(self, xml):
+        self._acao = 'enviar_lote'
+        self._xml = xml
+
+    def _post_header(self):
+        response = {
+            "content-type": "application/soap+xml; charset=utf-8;",
+            "Accept": "application/soap+xml; charset=utf-8;",
+        }
+        return response
+    
+    def _post(self, timeout=None):
+        certificado_a1 = CertificadoA1(self._certificado)
+        chave, cert = certificado_a1.separar_arquivo(self._certificado_senha,
+                                                     caminho=True)
+        chave_cert = (cert, chave)
+        try:
+            result = requests.post(
+                self.url,
+                self.xml,
+                headers=self._post_header(),
+                cert=chave_cert,
+                verify=False,
+                timeout=timeout,
+            )
+            result.encoding = "utf-8"
+            return result
+        except requests.exceptions.RequestException as e:
+            raise e
+        finally:
+            certificado_a1.excluir()
+
+    @property
+    def url(self):
+        return ''
+
+    @property
+    def xml(self):
+        return self._xml
+
+
---
+++ pynfe/processamento/nfse/sp_saoPaulo/comunicacao.py
@@ -0,0 +1,3 @@
+"""
+ajustar xml, url
+"""
